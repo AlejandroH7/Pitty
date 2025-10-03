4e564e50c05afed6ebd255c349a505cbd110b0ab
Rene Alejandro <rcastilloc9@miumg.edu.gt>
2025-10-02 22:33:53 -0600

    avance Docsso

--- src/main/java/com/pitty/infra/OpenApiConfig.java
+++ src/main/java/com/pitty/infra/OpenApiConfig.java
@@ -1,5 +1,26 @@
 package com.pitty.infra;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
 public class OpenApiConfig {
-    
+
+    @Bean
+    public OpenAPI pittyOpenAPI() {
+        var info = new Info()
+                .title("Pitty API")
+                .description("API REST del proyecto (Clientes, Ingredientes, Postres, Recetas, Pedidos)")
+                .version("v1.0.0")
+                .contact(new Contact().name("Equipo Pitty").email("equipo@pitty.local"));
+
+        var localServer = new Server().url("http://localhost:8080").description("Local");
+        return new OpenAPI().info(info).servers(List.of(localServer));
+    }
 }
--- src/main/java/com/pitty/infra/WebConfig.java
+++ src/main/java/com/pitty/infra/WebConfig.java
@@ -1,5 +1,16 @@
 package com.pitty.infra;
 
-public class WebConfig {
-    
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")   // ajusta si necesitas restringir
+                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+                .allowedHeaders("*");
+    }
 }
